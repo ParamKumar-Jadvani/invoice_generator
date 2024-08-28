4d0401be5dd3ce69a3bfe6a77eaa26e1c751ae46
Param-Kumar Jadvani <jadvaniparam@gmail.com>
2024-08-28 19:39:05 +0530

    Pdf Page Start

--- lib/screens/pdf_page.dart
+++ lib/screens/pdf_page.dart
@@ -1,4 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:invoice_generator/utils/globals.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import "package:printing/printing.dart";
 
 class PdfPage extends StatefulWidget {
   const PdfPage({super.key});
@@ -8,12 +14,38 @@ class PdfPage extends StatefulWidget {
 }
 
 class _PdfPageState extends State<PdfPage> {
+  Future<Uint8List> generateDocument(int index) async {
+    Map Data = Globals.userInvoiceData[index];
+    final pw.Document doc = pw.Document();
+
+    doc.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.standard,
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Text(
+              "${Data}",
+              style: pw.TextStyle(
+                fontSize: 20,
+                fontWeight: pw.FontWeight.bold,
+              ),
+            ),
+          ); // Center
+        },
+      ),
+    ); // Page
+    return doc.save();
+  }
+
   @override
   Widget build(BuildContext context) {
     int index = ModalRoute.of(context)!.settings.arguments as int;
     return Scaffold(
       appBar: AppBar(
         title: const Text("PDf Page"),
+      ),
+      body: PdfPreview(
+        build: (format) => generateDocument(index),
       ),
     );
   }
